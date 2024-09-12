36f38ebfd43b2c56bcf2585897f1ddc2b96260bd
Armando2405 <230i0048@martineztorre.tecnm.mx>
2024-09-12 13:44:51 -0600

    Add files via upload

---
+++ Ejemplo de lista 2.py
@@ -0,0 +1,15 @@
+# Definición de una lista vacía
+empty_list = []
+print("Lista vacía:", empty_list)
+
+# Definición de una lista de lenguajes de programación
+languages = ['Python', 'Ruby', 'Javascript']
+print("Lenguajes de programación:", languages)
+
+# Definición de una lista con los primeros números de la secuencia de Fibonacci
+fibonacci = [0, 1, 1, 2, 3, 5, 8, 13]
+print("Secuencia de Fibonacci:", fibonacci)
+
+# Definición de una lista con datos mixtos
+data = ['Tenerife', {'cielo': 'limpio', 'temp': 24}, 3718, (28.2933947, -16.5226597)]
+print("Datos mixtos:", data)
