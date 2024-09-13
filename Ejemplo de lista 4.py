b2d7869e154d0124e71ed74316b2eab4b92c82eb
Armando2405 <230i0048@martineztorre.tecnm.mx>
2024-09-12 21:19:10 -0600

    Add files via upload

---
+++ Ejemplo de lista 4.py
@@ -0,0 +1,8 @@
+# Define una lista con tres elementos
+l = [1, 2, 3]
+
+# Desempaqueta la lista en tres variables
+x, y, z = l
+
+# Imprime los valores de las variables x, y, z
+print(x, y, z)  # imprime 1 2 3
